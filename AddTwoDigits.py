54019ba9cb66aa42fbc832a2f8bbb28364f51ec5
anjali <sonam@asdfghjkl.lan>
2019-11-29 10:13:02 +0530

    Here some file added from 10days js from hackerrank:)

---
+++ AddTwoDigits.py
@@ -0,0 +1,6 @@
+def addTwoDigits(n):
+    division=n/10
+    modulus=n%10
+    result=division+modulus
+    return result
+addTwoDigits(29)    
---
+++ LargestNumber.py
@@ -0,0 +1,7 @@
+def largestNumber(n):
+    i=1
+    while i<=n:
+       multiply=i*"9"
+       i=i+1
+    return int(multiply)
+largestNumber(8)
