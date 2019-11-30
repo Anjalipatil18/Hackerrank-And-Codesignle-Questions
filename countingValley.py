02d8c519600dec92d1ec40856c088c143e979b60
anjali <sonam@asdfghjkl.lan>
2019-11-30 16:24:15 +0530

    In this code I did from hackerrank countingValleys

---
+++ countingValley.py
@@ -0,0 +1,11 @@
+def countingValleys(n, s):
+    level = 0
+    count = 0
+    for i in s:
+        if i=="U":
+            level+=1
+        if i=="D":
+            level-=1
+        if (level==0 and i=="U"):
+            count+=1
+    return count 
